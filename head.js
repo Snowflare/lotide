33c3d466e0d2fec58017f6a87ec544bbc37b6ad7
Daniel Zhang <danielzhang95@hotmail.com>
2020-04-28 01:10:09 +0000

    head added

---
+++ head.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  let prettyactual = actual;
+  let prettyexpected = expected;
+  if (typeof actual === 'string') {
+    prettyactual = '"' + actual + '"';
+  }
+  if (typeof expected === 'string') {
+    prettyexpected = '"' + expected + '"';
+  }
+
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) + `Assertion Passed: ${prettyactual} === ${prettyexpected}`);
+  } else {
+    console.log(String.fromCodePoint(0x1f6d1) + String.fromCodePoint(0x1f6d1) + String.fromCodePoint(0x1f6d1) + `Assertion Passed: ${prettyactual} !== ${prettyexpected}`);
+  }
+};
+const head = function(arr){
+  if (head.length === 0){
+    return undefined;
+  }
+  return arr[0];
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
