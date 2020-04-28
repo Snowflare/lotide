ba3ff45c2356f25c6dfcf06890323ee18e90dd06
Daniel Zhang <danielzhang95@hotmail.com>
2020-04-28 00:54:27 +0000

    assertEqual added

---
+++ assertEqual.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let prettyactual = actual;
+  let prettyexpected = expected;
+  if (typeof actual === 'string') {
+    prettyactual = '"' + actual + '"';
+  }
+  if (typeof expected === 'string') {
+    prettyexpected = '"' + expected + '"';
+  }
+
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) + 'Assertion Passed: ' + prettyactual + ' === ' + prettyexpected);
+  } else {
+    console.log(String.fromCodePoint(0x1f6d1) + String.fromCodePoint(0x1f6d1) + String.fromCodePoint(0x1f6d1) + 'Assertion Failed: ' + prettyactual + ' !== ' + prettyexpected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
