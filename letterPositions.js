780f0f19c4537eb5ea6d38f391c3c00d2f3571ba
Daniel Zhang <danielzhang95@hotmail.com>
2020-05-05 03:55:23 +0000

    letterPositions.js refactored

--- letterPositions.js
+++ letterPositions.js
@@ -1,27 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(actual,expected){
-  if (actual.length !== expected.length){
-    return false;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-  
-}
-const assertArraysEqual = function(actual, expected){
-  const tick = String.fromCodePoint(0x2705);
-  const cross = String.fromCodePoint(0x1f6d1);
-
-  if (eqArrays(actual, expected)) {
-    console.log(tick + tick + tick + `Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(cross + cross + cross + `Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
 const letterPositions = function(sentence) {
   const results = {};
   // logic to update results here
@@ -36,8 +13,5 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
-const result1 = letterPositions("lighthouse in the house");
-assertArraysEqual(result1.l,[0]);
-assertArraysEqual(result1.i,[1, 11]);
-assertArraysEqual(result1.g,[2]);
-assertArraysEqual(result1.h,[3, 5, 15, 18]);
+module.exports = letterPositions;
+
