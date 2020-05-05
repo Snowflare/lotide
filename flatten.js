bcfa05a916d7582cfc2885927983e6809528ba2f
Daniel Zhang <danielzhang95@hotmail.com>
2020-05-05 03:36:07 +0000

    flatten.js refactored

--- flatten.js
+++ flatten.js
@@ -1,27 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(actual,expected){
-  if (actual.length !== expected.length){
-    return false;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-  
-}
-const assertArraysEqual = function(actual, expected){
-  const tick = String.fromCodePoint(0x2705);
-  const cross = String.fromCodePoint(0x1f6d1);
-
-  if (eqArrays(actual, expected)) {
-    console.log(tick + tick + tick + `Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(cross + cross + cross + `Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
 const flatten = function(arr){
   let res = []
   for (let i of arr){
@@ -33,5 +10,5 @@ const flatten = function(arr){
   }
   return res;
 }
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-console.log(flatten([1, 2, [3, 4, [[[[[4]]]]]], 5, [6]]));
+
+module.exports = flatten;
