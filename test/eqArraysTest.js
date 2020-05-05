ac0524a38956feb88c5bbcbb28f45b34e1e4d7da
Daniel Zhang <danielzhang95@hotmail.com>
2020-05-05 04:35:29 +0000

    eqArraysTest.js added

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,16 +1,43 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 // TEST CODE
-assertEqual(eqArrays("Lighthouse Labs", "Bootcamp"), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+describe("#eqObjects", () => {
+  it("returns false for eqArrays('Lighthouse Labs', 'Bootcamp')", () => {
+    assert.strictEqual(eqArrays("Lighthouse Labs", "Bootcamp"), false);
+  });
+  it("returns true for eqArrays([1, 2, 3], [1, 2, 3])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns false for eqArrays([1, 2, 3], [3, 2, 1])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);// => false
+  });
+  it("returns true for eqArrays(['1', '2', '3'], ['1', '2', '3'])", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
+  });
+  it("returns false for eqArrays(['1', '2', '3'], ['1', '2', 3])", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+  });
+  it("returns false for eqArrays([1, 2], [1, 2, 3])", () => {
+    assert.strictEqual(eqArrays([1, 2], [1, 2, 3]), false);
+  });
+  it("returns false for eqArrays([1, 2, 3], [1, 2])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2]), false);
+  });
+  it("returns false for eqArrays([1, 2, undefined], [1, 2])", () => {
+    assert.strictEqual(eqArrays([1, 2, undefined], [1, 2]), false);
+  });
+});
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true );// => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);// => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
-assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays([1, 2, undefined], [1, 2]), false);
+
+
+
+
+
+
+
+
+
+
