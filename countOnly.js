ca9de07ee342f99b75cf76be698f1aa3facb6580
Daniel Zhang <danielzhang95@hotmail.com>
2020-05-05 03:45:37 +0000

    countOnly.js refactored

--- countOnly.js
+++ countOnly.js
@@ -1,21 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  let prettyactual = actual;
-  let prettyexpected = expected;
-  if (typeof actual === 'string') {
-    prettyactual = '"' + actual + '"';
-  }
-  if (typeof expected === 'string') {
-    prettyexpected = '"' + expected + '"';
-  }
-
-  if (actual === expected) {
-    console.log(String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) + `Assertion Passed: ${prettyactual} === ${prettyexpected}`);
-  } else {
-    console.log(String.fromCodePoint(0x1f6d1) + String.fromCodePoint(0x1f6d1) + String.fromCodePoint(0x1f6d1) + `Assertion Failed: ${prettyactual} !== ${prettyexpected}`);
-  }
-};
-
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
@@ -32,20 +14,5 @@ const countOnly = function(allItems, itemsToCount) {
   return res;
 }
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
+module.exports = countOnly;
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
