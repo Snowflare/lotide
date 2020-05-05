6675ac6520832429ba759178af51bce46ffc6472
Daniel Zhang <danielzhang95@hotmail.com>
2020-05-05 04:03:21 +0000

    findKeyByValueTest.js added

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,23 @@
+const assert = require('chai').assert;
+const findKeyByValue = require('../findKeyByValue');
+
+describe("#findKeyByValue", () => {
+  const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  it("returns 'drama' for bestTVShowsByGenre, 'The Wire' ", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama');
+  });
+  it("returns undefined for bestTVShowsByGenre, 'That 70s Show' ", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, 'That 70s Show'), undefined);
+  });
+  it("returns 'sci_fi' for bestTVShowsByGenre, 'The Expanse' ", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, 'The Expanse'), 'sci_fi');
+  });
+});
+
+
+
+
